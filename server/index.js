05cd4c8fd6e8ff0f1180e6873002f8d935b91aa9
thanhNha <thanhna.k13@gmail.com>
2023-10-05 23:45:28 +0700

    file vercel builds deploy vercel

--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/api/auth', authRouter);
 app.use('/api/todos', todoRouter);
 
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,6 +3,9 @@ const AuthController = require('../controllers/authController')
 const router = express.Router();
 
 /* GET users listing. */
+router.get('/users',(req,res)=>{
+  res.json('demo deployment')
+})
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 
